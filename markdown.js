e250515df4e8a80142b3283c1a1ee0252225dafd
Andrew Richardson <65513935+Anubis1096@users.noreply.github.com>
2020-08-05 22:29:21 -0700

    Update markdown.js

--- markdown.js
+++ markdown.js
@@ -1,29 +1,36 @@
 // Generate markdown for README
 function markdown(data) {
-    return `# ${data.title}
-    ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+return `# ${data.title}
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
 
-    ## Description:
-     ${data.description}
-    ## Table of contents:
+## Description:
+    ${data.description}
+
+## Table of contents:
     * Installation
     * Usage
     * License
     * Contributing
     * Tests
     * Questions
-    ## Installation:
-     ${data.installation}
-    ## Usage:
-     ${data.usage}
-    ## License:
-     ${data.license}
-    ## Contributing:
-     ${data.contributing}
-    ## Tests: 
-     ${data.tests}
-    ## Questions: 
-     ${data.questions}
+    
+## Installation:
+    ${data.installation}
+
+## Usage:
+    ${data.usage}
+
+## License:
+    ${data.license}
+
+## Contributing:
+    ${data.contributing}
+
+## Tests: 
+    ${data.tests}
+
+## Questions: 
+    ${data.questions}
   `;
   }
   
